1a3b86b5582cc870d593e1bf1745b9bf02c08fc8
altern <sergey.shmarkatyuk@gmail.com>
2014-01-23 22:25:11 -0800

    changed database name to touchdevelop

--- api/config.php
+++ api/config.php
@@ -1,7 +1,7 @@
 <?php
 	error_reporting(0);
 	$conn = mysql_connect("localhost", 'root', 'My5ql123');
-	$db = mysql_select_db('cs519');
+	$db = mysql_select_db('touchdevelop');
     set_time_limit(0);   
     ini_set('mysql.connect_timeout','0');   
     ini_set('max_execution_time', '0');
