33348f337618081368720dd8da95757bca061fad
altern <sergey.shmarkatyuk@gmail.com>
2014-01-25 00:24:22 -0800

    updated database init script

--- sql/db_structure.sql
+++ sql/db_structure.sql
@@ -1,94 +1,168 @@
--- phpMyAdmin SQL Dump
--- version 3.2.0.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Nov 04, 2013 at 10:54 AM
--- Server version: 5.1.37
--- PHP Version: 5.3.0
+CREATE TABLE authors (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name TINYTEXT NOT NULL,
+  author_id TINYTEXT NOT NULL,
+  join_date DATETIME NULL,
+  features INTEGER UNSIGNED NULL,
+  activedays INTEGER UNSIGNED NULL,
+  receivedpositivereviews INTEGER UNSIGNED NULL,
+  subscribers INTEGER UNSIGNED NULL,
+  score INTEGER UNSIGNED NULL,
+  PRIMARY KEY(id)
+);
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+CREATE TABLE authors_learned_features (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  author_id INTEGER UNSIGNED NULL,
+  feature_id INTEGER UNSIGNED NULL,
+  PRIMARY KEY(id)
+);
 
---
--- Database: `cs519`
---
+CREATE TABLE chunks (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  content TEXT NOT NULL,
+  PRIMARY KEY(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE features (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name TEXT NULL,
+  description TEXT NULL,
+  source TEXT NULL,
+  PRIMARY KEY(id)
+);
 
---
--- Table structure for table `authors`
---
+CREATE TABLE hashtags (
+  id INTEGER(11) NOT NULL AUTO_INCREMENT,
+  name TINYTEXT NOT NULL,
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE IF NOT EXISTS `authors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` tinytext NOT NULL,
-  `author_id` tinytext NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+CREATE TABLE libraries (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name TEXT NULL,
+  library_id TINYTEXT NULL,
+  description TEXT NULL,
+  PRIMARY KEY(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE pages (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name TEXT NULL,
+  url_id TEXT NULL,
+  script_id TINYTEXT NULL,
+  PRIMARY KEY(id)
+);
 
---
--- Table structure for table `chunks`
---
+CREATE TABLE scripts (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name TINYTEXT NOT NULL,
+  content TEXT NOT NULL,
+  date DATETIME NOT NULL,
+  script_id TINYTEXT NOT NULL,
+  author_id INTEGER(11) NOT NULL,
+  description TEXT NOT NULL,
+  positivereviews INTEGER UNSIGNED NULL,
+  cumulativepositivereviews INTEGER UNSIGNED NULL,
+  installations INTEGER UNSIGNED NULL,
+  runs INTEGER UNSIGNED NULL,
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE IF NOT EXISTS `chunks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=46 ;
+CREATE TABLE scripts_chunks (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  script_id INTEGER(11) NOT NULL,
+  chunk_id INTEGER(11) NOT NULL,
+  seq INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE scripts_features (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  script_id INTEGER UNSIGNED NOT NULL,
+  feature_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(id)
+);
 
---
--- Table structure for table `hashtags`
---
+CREATE TABLE scripts_hashtags (
+  id INTEGER(11) NOT NULL AUTO_INCREMENT,
+  script_id INTEGER(11) NOT NULL,
+  hashtag_id INTEGER(11) NOT NULL,
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE IF NOT EXISTS `hashtags` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` tinytext NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+CREATE TABLE scripts_history (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  script_id INTEGER UNSIGNED NULL,
+  base_id INTEGER UNSIGNED NULL,
+  successor_id INTEGER UNSIGNED NULL,
+  author_id INTEGER UNSIGNED NULL,
+  node_id INTEGER UNSIGNED NULL,
+  date DATETIME NULL,
+  PRIMARY KEY(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE scripts_libraries (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  script_id INTEGER UNSIGNED NOT NULL,
+  library_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(id)
+);
 
---
--- Table structure for table `scripts`
---
+CREATE TABLE scripts_scores (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  script_id INTEGER UNSIGNED NULL,
+  positivereviews INTEGER UNSIGNED NULL,
+  cumulativepositivereviews INTEGER UNSIGNED NULL,
+  installations INTEGER UNSIGNED NULL,
+  runs INTEGER UNSIGNED NULL,
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE IF NOT EXISTS `scripts` (
-  `name` tinytext NOT NULL,
-  `content` text NOT NULL,
-  `date` datetime NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `script_id` tinytext NOT NULL,
-  `author_id` int(11) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=52 ;
+CREATE TABLE scripts_tutorials (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  script_id INTEGER UNSIGNED NOT NULL,
+  tutorial_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE tutorials (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name TEXT NULL,
+  script_id INTEGER UNSIGNED NULL,
+  is_interactive BOOL NULL,
+  PRIMARY KEY(id)
+);
 
---
--- Table structure for table `script_chunks`
---
+CREATE TABLE tutorials_by_author (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  author_id INTEGER UNSIGNED NOT NULL,
+  tutorial_id INTEGER UNSIGNED NOT NULL,
+  is_completed BOOL NULL,
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE IF NOT EXISTS `script_chunks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `script_id` int(11) NOT NULL,
-  `chunk_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=46 ;
+CREATE TABLE tutorials_features (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  tutorial_id INTEGER UNSIGNED NOT NULL,
+  feature_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE tutorials_libraries (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  tutorial_id INTEGER UNSIGNED NOT NULL,
+  library_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY(id)
+);
 
---
--- Table structure for table `script_hashtags`
---
+CREATE TABLE tutorials_pages (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  seq INTEGER UNSIGNED NULL,
+  tutorial_id INTEGER UNSIGNED NULL,
+  page_id INTEGER UNSIGNED NULL,
+  category_name TEXT NULL,
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE IF NOT EXISTS `script_hashtags` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `script_id` int(11) NOT NULL,
-  `hashtag_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=96 ;
+
