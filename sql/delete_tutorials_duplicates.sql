56bb01f51831e9282b1236f7694141be9af6761b
altern <sergey.shmarkatyuk@gmail.com>
2014-01-27 19:45:16 -0800

    updated script to delete all tutorial duplicates instead of subset of tutorial duplicates

--- sql/delete_tutorials_duplicates.sql
+++ sql/delete_tutorials_duplicates.sql
@@ -10,12 +10,12 @@ FROM (
 SELECT COUNT( * ) c, MAX( s.date ) max_date, t.name, t.is_interactive
 FROM tutorials t
 JOIN scripts s ON s.id = t.script_id
-WHERE t.id
-IN (
+-- WHERE t.id
+-- IN (
 
-SELECT tutorial_id
-FROM scripts_tutorials
-)
+-- SELECT tutorial_id
+-- FROM scripts_tutorials
+-- )
 GROUP BY t.name, t.is_interactive
 HAVING c >1
 ORDER BY c DESC
@@ -41,12 +41,12 @@ FROM (
 SELECT COUNT( * ) c, MAX( s.date ) max_date, t.name, t.is_interactive
 FROM tutorials t
 JOIN scripts s ON s.id = t.script_id
-WHERE t.id
-IN (
+-- WHERE t.id
+-- IN (
 
-SELECT tutorial_id
-FROM scripts_tutorials
-)
+-- SELECT tutorial_id
+-- FROM scripts_tutorials
+-- )
 GROUP BY t.name, t.is_interactive
 HAVING c >1
 ORDER BY c DESC
@@ -72,12 +72,12 @@ FROM (
 SELECT COUNT( * ) c, MAX( s.date ) max_date, t.name, t.is_interactive
 FROM tutorials t
 JOIN scripts s ON s.id = t.script_id
-WHERE t.id
-IN (
+-- WHERE t.id
+-- IN (
 
-SELECT tutorial_id
-FROM scripts_tutorials
-)
+-- SELECT tutorial_id
+-- FROM scripts_tutorials
+-- )
 GROUP BY t.name, t.is_interactive
 HAVING c >1
 ORDER BY c DESC
@@ -102,12 +102,12 @@ FROM (
 SELECT COUNT( * ) c, MAX( s.date ) max_date, t.name, t.is_interactive
 FROM tutorials t
 JOIN scripts s ON s.id = t.script_id
-WHERE t.id
-IN (
+-- WHERE t.id
+-- IN (
 
-SELECT tutorial_id
-FROM scripts_tutorials
-)
+-- SELECT tutorial_id
+-- FROM scripts_tutorials
+-- )
 GROUP BY t.name, t.is_interactive
 HAVING c >1
 ORDER BY c DESC
@@ -122,30 +122,22 @@ AND t2.script_id = s2.id-- and s2.id in (select script_id from scripts_tutorials
 
 DELETE
 FROM tutorials
-WHERE id
-IN (
-SELECT 
-t2.id tutorial_id
+WHERE id IN (
+	select tmp.tutorial_id from (
+		SELECT 
+		t2.id tutorial_id
+		FROM (
 
-FROM (
-
-SELECT COUNT( * ) c, MAX( s.date ) max_date, t.name, t.is_interactive
-FROM tutorials t
-JOIN scripts s ON s.id = t.script_id
-WHERE t.id
-IN (
-
-SELECT tutorial_id
-FROM scripts_tutorials
-)
-GROUP BY t.name, t.is_interactive
-HAVING c >1
-ORDER BY c DESC
-) AS s1
-JOIN tutorials t2 ON t2.name = s1.name
-AND t2.is_interactive = s1.is_interactive
-JOIN scripts s2 ON s2.date < s1.max_date
-AND t2.script_id = s2.id-- and s2.id in (select script_id from scripts_tutorials)
--- group by s1.name, t2.is_interactive
--- order by c1 desc
+			SELECT COUNT( * ) c, MAX( s.date ) max_date, t.name, t.is_interactive
+			FROM tutorials t
+			JOIN scripts s ON s.id = t.script_id
+			GROUP BY t.name, t.is_interactive
+			HAVING c >1
+			ORDER BY c DESC
+		) AS s1
+		JOIN tutorials t2 ON t2.name = s1.name
+		AND t2.is_interactive = s1.is_interactive
+		JOIN scripts s2 ON s2.date < s1.max_date
+		AND t2.script_id = s2.id 
+	) as tmp
 )
